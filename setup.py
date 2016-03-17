21f5f0f101377143620fe7246f37ea0eb45b7d4e
Yu Liang <yu.liang@thekono.com>
2016-03-17 16:19:57 +0800

    Add bin/imgoptimize to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # If you had imgutil.tests, you would also include that in this list
     packages=find_packages(),
     # Any executable scripts, typically in 'bin'. E.g 'bin/do-something.py'
-    scripts=[],
+    scripts=['bin/imgoptimize'],
     # REQUIRED: Your project's URL
     url='',
     # Put your license here. See LICENSE.txt for more information
